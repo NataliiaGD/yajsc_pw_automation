efc28c4719362656855721945e6503bff0f4e057
NataliiaGD <nataliagordiyenko8@gmail.com>
2025-08-18 23:59:24 +0300

    added unit-11 tasks

---
+++ pages/account.page.ts
@@ -0,0 +1,12 @@
+import { Locator, Page } from "@playwright/test";
+export class AccountPage {
+    page: Page;
+    title: Locator;
+
+    constructor(page: Page){
+        this.page = page;
+        this.title = this.page.getByTestId('page-title');
+    }
+
+
+}
--- tests/tests.spec.ts
+++ tests/tests.spec.ts
@@ -1,19 +1,22 @@
 import { test, expect } from '@playwright/test';
+import { LoginPage } from '../pages/login.page';
+import { AccountPage } from '../pages/account.page';
+import { Header } from '../fragments/header.fragment';
 
 test.skip(!!process.env.CI, 'Test is skipped in CI');
 
-const login = {
-    email: 'customer@practicesoftwaretesting.com',
-    password: 'welcome01'
-};
 
 test('login', async ({page}) => {
+    const loginPage = new LoginPage(page);
+    const accountPage = new AccountPage(page);
+    const headerFragment = new Header(page);
     await page.goto('/auth/login');
-    await page.getByTestId('email').fill(login.email);
-    await page.getByTestId('password').fill(login.password);
-    await page.getByTestId('login-submit').click();
+    await loginPage.login('customer@practicesoftwaretesting.com','welcome01');
+    // await page.getByTestId('email').fill(login.email);
+    // await page.getByTestId('password').fill(login.password);
+    // await page.getByTestId('login-submit').click();
     await expect(page).toHaveURL('/account');
-    await expect(page.getByTestId('page-title')).toHaveText('My account');
-    await expect(page.getByTestId('nav-menu')).toHaveText(' Jane Doe ');
+    await expect(accountPage.title).toHaveText('My account');
+    await expect(headerFragment.menu).toHaveText(' Jane Doe ');
     
 });
